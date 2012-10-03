ddcfef5d1e8df6425db0c4f9c0c29308bac6696d
Dmitry Ryumin <ryumin@zoom-group.ru>
2012-10-04 00:31:59 +0700

    sample config

---
+++ config-sample.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://topraise.local/');
+define('HTTP_IMAGE', 'http://topraise.local/image/');
+define('HTTP_ADMIN', 'http://topraise.local/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://topraise.local/');
+define('HTTPS_IMAGE', 'http://topraise.local/image/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\works\topraise\www/catalog/');
+define('DIR_SYSTEM', 'D:\works\topraise\www/system/');
+define('DIR_DATABASE', 'D:\works\topraise\www/system/database/');
+define('DIR_LANGUAGE', 'D:\works\topraise\www/catalog/language/');
+define('DIR_TEMPLATE', 'D:\works\topraise\www/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\works\topraise\www/system/config/');
+define('DIR_IMAGE', 'D:\works\topraise\www/image/');
+define('DIR_CACHE', 'D:\works\topraise\www/system/cache/');
+define('DIR_DOWNLOAD', 'D:\works\topraise\www/download/');
+define('DIR_LOGS', 'D:\works\topraise\www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'topraise');
+define('DB_PREFIX', '');
+?>

5dc4bfdde8aee581abc0f4364ada315b5d38fde8
Dmitry Ryumin <dmitryryumin@Dmitry-Ryumins-MacBook-Pro.local>
2012-10-14 22:10:33 +0700

    added config

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://topraise.local/admin/');
+define('HTTP_CATALOG', 'http://topraise.local/');
+define('HTTP_IMAGE', 'http://topraise.local/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://topraise.local/admin/');
+define('HTTPS_CATALOG', 'http://topraise.local/');
+define('HTTPS_IMAGE', 'http://topraise.local/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/dmitryryumin/works/topraise/topraise/admin/');
+define('DIR_SYSTEM', '/Users/dmitryryumin/works/topraise/topraise/system/');
+define('DIR_DATABASE', '/Users/dmitryryumin/works/topraise/topraise/system/database/');
+define('DIR_LANGUAGE', '/Users/dmitryryumin/works/topraise/topraise/admin/language/');
+define('DIR_TEMPLATE', '/Users/dmitryryumin/works/topraise/topraise/admin/view/template/');
+define('DIR_CONFIG', '/Users/dmitryryumin/works/topraise/topraise/system/config/');
+define('DIR_IMAGE', '/Users/dmitryryumin/works/topraise/topraise/image/');
+define('DIR_CACHE', '/Users/dmitryryumin/works/topraise/topraise/system/cache/');
+define('DIR_DOWNLOAD', '/Users/dmitryryumin/works/topraise/topraise/download/');
+define('DIR_LOGS', '/Users/dmitryryumin/works/topraise/topraise/system/logs/');
+define('DIR_CATALOG', '/Users/dmitryryumin/works/topraise/topraise/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'topraise');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://topraise.local/');
+define('HTTP_IMAGE', 'http://topraise.local/image/');
+define('HTTP_ADMIN', 'http://topraise.local/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://topraise.local/');
+define('HTTPS_IMAGE', 'http://topraise.local/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/dmitryryumin/works/topraise/topraise/catalog/');
+define('DIR_SYSTEM', '/Users/dmitryryumin/works/topraise/topraise/system/');
+define('DIR_DATABASE', '/Users/dmitryryumin/works/topraise/topraise/system/database/');
+define('DIR_LANGUAGE', '/Users/dmitryryumin/works/topraise/topraise/catalog/language/');
+define('DIR_TEMPLATE', '/Users/dmitryryumin/works/topraise/topraise/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/dmitryryumin/works/topraise/topraise/system/config/');
+define('DIR_IMAGE', '/Users/dmitryryumin/works/topraise/topraise/image/');
+define('DIR_CACHE', '/Users/dmitryryumin/works/topraise/topraise/system/cache/');
+define('DIR_DOWNLOAD', '/Users/dmitryryumin/works/topraise/topraise/download/');
+define('DIR_LOGS', '/Users/dmitryryumin/works/topraise/topraise/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'topraise');
+define('DB_PREFIX', '');
+?>
